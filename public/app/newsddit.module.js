cd08dd898873b4651ade1f29cd9c8b3ccf0565ca
nicehatsoftware <fernando.reyes@nicehatsoftware.com>
2023-06-23 09:26:58 -0600

    added icons to upvotes and comment buttons

--- public/app/newsddit.module.js
+++ public/app/newsddit.module.js
@@ -106,14 +106,19 @@ const newsdditModule = (function () {
                             <p class="card-text"><small>by <em>${posts[i].author}</em></small></p>
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
-                                    <button type="button" class="btn btn-sm btn-outline-secondary"><small>Up votes: ${posts[i].ups}</small></button>
+                                    <button type="button" class="btn btn-sm btn-outline-secondary">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+                                        </svg>
+                                        <small>${posts[i].ups}</small>
+                                    </button>
                                     ${link_text}
-                                    <a class="btn btn-warning" href="https://reddit.com/${posts[i].comments}" target="_blank"><small>Comments: ${posts[i].num_comments}</small></a></small>
+                                    <a class="btn btn-sm btn-outline-secondary" href="https://reddit.com/${posts[i].comments}" target="_blank">
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
+                                        <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
+                                        </svg>
+                                        <small>${posts[i].num_comments}</small></a></small>
                                 </div>
-                                <!--
-                                <small class="text-body-secondary"><a class="btn btn-warning" href="https://reddit.com/${posts[i].comments}" target="_blank"><small>Comments: ${posts[i].num_comments}</small></a></small>
-
-                                -->
                             </div>
                     </div>
                     </div>
